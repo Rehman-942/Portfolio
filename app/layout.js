278dee63d74323efa52ca13a9f582f80e61e3518
Rehman-942 <rehmanafzal942@gmail.com>
2024-09-22 11:42:54 +0500

    added google tags corrected

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,11 @@
 import "@css/plugins/bootstrap-grid.css";
 import { Jost } from "next/font/google";
 import "./globals.css";
-
 import "@css/plugins/magnific-popup.css";
 import "@css/plugins/swiper.min.css";
 import "@fonts/font-awesome/css/all.min.css";
 import Head from "next/head";
-
+import Script from "next/script";
 import "@css/style.css";
 
 const jost = Jost({
@@ -15,7 +14,7 @@ const jost = Jost({
   subsets: ['latin'],
   variable: '--font-jost',
   display: 'swap',
-})
+});
 
 export const metadata = {
   title: "Rehman Afzal - Portfolio",
@@ -25,21 +24,23 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${jost.variable}`}>
-       <Head>
-        {/* Google Analytics */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-R31P3LD5MH"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-R31P3LD5MH');
-            `,
-          }}
-        />
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
       </Head>
-      <body className="mil-custom-scroll">{children}</body>
+      <body className="mil-custom-scroll">
+        {children}
+        {/* Google Analytics */}
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-R31P3LD5MH" />
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-R31P3LD5MH');
+          `}
+        </Script>
+      </body>
     </html>
   );
 }
